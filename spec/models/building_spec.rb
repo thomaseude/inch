c9247567db06c7c52cdf07d7b04d093a223e31fc
Thomas <tmmeude@gmail.com>
2023-11-28 10:22:44 +0100

    add rspec

--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -11,20 +11,60 @@ RSpec.describe Building, type: :model do
     end
   end
 
+  let(:csv_create) do
+    require 'csv'
+    csv_string = CSV.generate do |csv|
+      csv << ["reference","address","zip_code","city","country","manager_name"]
+      csv << ["3","11 Rue La bruyère","75009","Paris","France","Martin Faure"]
+      csv << ["4","10 Rue La bruyère","75010","Paris","France","Martin Faure"]
+      csv << ["5","10 Rue La bruyère","75009","Le Havre","France","Martin Faure"]
+      csv << ["6","10 Rue La bruyère","75009","Paris","Spain","Martin Faure"]
+      csv << ["7","10 Rue La bruyère","75009","Paris","France","Martin Faure"]
+    end
+  end
+
+  let(:csv_update) do
+    require 'csv'
+    csv_string = CSV.generate do |csv|
+      csv << ["2","12  rue de la boétie","75008","Paris","France","Martin Faure"] #update all when reference exist
+      csv << ["1","10 Rue La bruyère","75009","Paris","France","Thomas EUDE"] #update manager_name when address exist
+    end
+  end
+
 
 
   describe 'Import CSV' do
     context "Create Building instance if different address (street + zip + city + country)" do
       it "CREATE Building" do
-        # if street is not found
-        # if zip is not found
-        # if city is not found
-        # if country is not found
+        # import init => create
+        Building.import(csv_file)
+        expect(Building.count).to eq(2)
+
+        # Change info into address => create
+        Building.import(csv_create)
+        expect(Building.count).to eq(7)
+
+        expect(Building.first.reference).to eq("1")
+        expect(Building.first.address).to eq("10 Rue La bruyère")
+        expect(Building.first.zip_code).to eq("75009")
+        expect(Building.first.city).to eq("Paris")
+        expect(Building.first.country).to eq("France")
+        expect(Building.first.manager_name).to eq("Martin Faure")
       end
     end
 
     context "Update Building instance if address (street + zip + city + country) is the same" do
       it "UPDATE Building" do
+        Building.import(csv_update)
+
+        expect(Building.count).to eq(7)
+
+        expect(Building.first.reference).to eq("1")
+        expect(Building.first.address).to eq("10 Rue La bruyère")
+        expect(Building.first.zip_code).to eq("75009")
+        expect(Building.first.city).to eq("Paris")
+        expect(Building.first.country).to eq("France")
+        expect(Building.first.manager_name).to eq("Thomas EUDE")
       end
     end
   end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,4 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
+
+  let(:csv_file) do
+    require "csv"
+    CSV.generate do |csv|
+      csv << ["reference","firstname","lastname","home_phone_number","mobile_phone_number","email","address"]
+      csv << ["1","Henri","Dupont","0123456789","0623456789","h.dupont@gmail.com","10 Rue La bruyère"]
+      csv << ["2","Jean","Durand","0123336789","0663456789","jdurand@gmail.com","40 Rue René Clair"]
+    end
+  end
+
+  let(:csv_create) do
+    require "csv"
+    CSV.generate do |csv|
+      csv << ["reference","firstname","lastname","home_phone_number","mobile_phone_number","email","address"]
+      csv << ["3","Henri","Dupont","0123456789","0623456789","h.dupont@gmail.com","10 Rue La bruyère"]
+      csv << ["4","Thomas","Durand","0123336789","0663456789","jdurand@gmail.com","40 Rue René Clair"]
+      csv << ["5","Thomas","EUDE","0123336789","0663456789","jdurand@gmail.com","40 Rue René Clair"]
+    end
+  end
+
+  let(:csv_update) do
+    require "csv"
+    CSV.generate do |csv|
+      csv << ["reference","firstname","lastname","home_phone_number","mobile_phone_number","email","address"]
+      csv << ["1","Pierre","Dupont","0123456789","0623456789","h.dupont@gmail.com","10 Rue La bruyère"]
+      csv << ["2","Jean","Durand","0102030405","0663456789","jdurand@gmail.com","40 Rue René Clair"]
+      csv << ["2","Jean","Durand","0102030405","0602030405","jdurand@gmail.com","40 Rue René Clair"]
+      csv << ["2","Jean","Durand","0102030405","0602030405","jean-durand@gmail.com","40 Rue René Clair"]
+      csv << ["2","Jean","Durand","0102030405","0602030405","jean-durand@gmail.com","42 Rue René Clair"]
+    end
+  end
+
+  describe 'Import CSV' do
+    context "Create Person instance if different address (street + zip + city + country)" do
+      it "CREATE Person" do
+        # import init => create
+        Person.import(csv_file)
+        expect(Person.count).to eq(2)
+
+        # Change info into address => create
+        Person.import(csv_create)
+        expect(Person.count).to eq(5)
+
+        expect(Person.first.reference).to eq("1")
+        expect(Person.first.firstname).to eq("Henri")
+        expect(Person.first.lastname).to eq("Dupont")
+        expect(Person.first.home_phone_number).to eq("0123456789")
+        expect(Person.first.mobile_phone_number).to eq("0623456789")
+        expect(Person.first.email).to eq("h.dupont@gmail.com")
+        expect(Person.first.address).to eq("10 Rue La bruyère")
+      end
+    end
+
+    context "Update Person instance if address (street + zip + city + country) is the same" do
+      it "UPDATE Person" do
+        Person.import(csv_update)
+
+        expect(Person.count).to eq(5)
+
+        expect(Person.first.reference).to eq("1")
+        expect(Person.first.firstname).to eq("Pierre")
+        expect(Person.first.lastname).to eq("Dupont")
+        expect(Person.first.home_phone_number).to eq("0123456789")
+        expect(Person.first.mobile_phone_number).to eq("0623456789")
+        expect(Person.first.email).to eq("h.dupont@gmail.com")
+        expect(Person.first.address).to eq("10 Rue La bruyère")
+      end
+    end
+  end
 end
